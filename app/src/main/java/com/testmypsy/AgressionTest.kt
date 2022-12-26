71d338fa348412b73de7d3dcb52874826a3bab1f
unknown <rajsagare14@gmail.com>
2022-12-26 11:31:59 +0530

    XML For Questionaire Completed and Tested

--- app/src/main/java/com/testmypsy/AgressionTest.kt
+++ app/src/main/java/com/testmypsy/AgressionTest.kt
@@ -2,10 +2,12 @@ package com.testmypsy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AgressionTest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agression_test)
+        val submitButton = findViewById<Button>(R.id.)
     }
 }
